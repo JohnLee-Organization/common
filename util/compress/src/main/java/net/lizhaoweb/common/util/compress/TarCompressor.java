0280dd9fee7044f9340f36cb066b661c821c1d82
lz958942 <404644381@qq.com>
2017-08-10 21:04:55 +0800

    增加以 tar 格式压缩/解压的功能

---
+++ util/compress/src/main/java/net/lizhaoweb/common/util/compress/TarCompressor.java
@@ -0,0 +1,114 @@
+/**
+ * Copyright (c) 2017, Stupid Bird and/or its affiliates. All rights reserved.
+ * STUPID BIRD PROPRIETARY/CONFIDENTIAL. Use is subject to license terms.
+ *
+ * @Project : common
+ * @Package : net.lizhaoweb.common.util.compress
+ * @author <a href="http://www.lizhaoweb.net">李召(John.Lee)</a>
+ * @EMAIL 404644381@qq.com
+ * @Time : 20:25
+ */
+package net.lizhaoweb.common.util.compress;
+
+import org.apache.commons.compress.archivers.tar.TarArchiveEntry;
+import org.apache.commons.compress.archivers.tar.TarArchiveOutputStream;
+import org.apache.commons.io.IOUtils;
+
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
+
+/**
+ * <h1>压缩工具 - Tar</h1>
+ *
+ * @author <a href="http://www.lizhaoweb.cn">李召(John.Lee)</a>
+ * @version 1.0.0.0.1
+ * @EMAIL 404644381@qq.com
+ * @notes Created on 2017年08月10日<br>
+ * Revision of last commit:$Revision$<br>
+ * Author of last commit:$Author$<br>
+ * Date of last commit:$Date$<br>
+ */
+public class TarCompressor extends AbstractCompressOrDecompress {
+
+    private int countRecursive = 1; // 定义递归次数变量
+
+    public TarCompressor(boolean verbose) {
+        super(verbose);
+    }
+
+    /**
+     * 压缩
+     *
+     * @param inputFileOrDir 被压缩的文件或目录
+     * @param tarFile        压缩文件
+     * @throws Exception 异常
+     */
+    public void compress(String inputFileOrDir, String tarFile) throws Exception {
+        this.compress(new File(inputFileOrDir), new File(tarFile));
+    }
+
+    /**
+     * 压缩
+     *
+     * @param inputFileOrDir 被压缩的文件或目录
+     * @param tarFile        压缩文件
+     * @throws Exception 异常
+     */
+    public void compress(File inputFileOrDir, File tarFile) throws Exception {
+        FileOutputStream fileOutputStream = null;
+        TarArchiveOutputStream tarArchiveOutputStream = null;
+        try {
+            this.printInformation(String.format("The file[%s] for zip is compressing ...", tarFile));
+            fileOutputStream = new FileOutputStream(tarFile);
+            tarArchiveOutputStream = new TarArchiveOutputStream(fileOutputStream);
+            this.compress(tarArchiveOutputStream, inputFileOrDir, inputFileOrDir.getName());
+//            tarArchiveOutputStream.flush();
+//            tarArchiveOutputStream.finish();
+//            tarArchiveOutputStream.closeArchiveEntry();
+        } finally {
+            IOUtils.closeQuietly(tarArchiveOutputStream);// 输出流关闭
+            IOUtils.closeQuietly(fileOutputStream);// 输出流关闭
+        }
+        this.printInformation(String.format("The file[%s] for zip is compressed", tarFile));
+    }
+
+    private void compress(TarArchiveOutputStream tarArchiveOutputStream, File file, String base) throws Exception { // 方法重载
+        if (file.isDirectory()) {
+            File[] files = file.listFiles();
+            if (files == null) {
+                return;
+            }
+            String baseDir = null;
+            if (base.endsWith("/")) {
+                baseDir = base;
+            } else {
+                baseDir = String.format("%s/", base);
+            }
+            if (files.length == 0) {
+                TarArchiveEntry tarArchiveEntry = new TarArchiveEntry(baseDir);
+                tarArchiveOutputStream.putArchiveEntry(tarArchiveEntry);
+                tarArchiveOutputStream.closeArchiveEntry();
+                this.printInformation(baseDir);
+            }
+            for (File childFile : files) {
+                this.compress(tarArchiveOutputStream, childFile, baseDir + childFile.getName()); // 递归遍历子文件夹
+            }
+            this.printInformation(String.format("No. %d recursion", countRecursive));
+            countRecursive++;
+        } else {
+            FileInputStream fileInputStream = null;
+            try {
+                TarArchiveEntry tarArchiveEntry = new TarArchiveEntry(base);
+                tarArchiveEntry.setSize(file.length());
+                tarArchiveOutputStream.putArchiveEntry(tarArchiveEntry);
+                this.printInformation(base);
+                fileInputStream = new FileInputStream(file);
+                IOUtils.copy(fileInputStream, tarArchiveOutputStream);
+            } finally {
+                IOUtils.closeQuietly(fileInputStream);// 输入流关闭
+                tarArchiveOutputStream.closeArchiveEntry();
+            }
+        }
+    }
+}
---
+++ util/compress/src/main/java/net/lizhaoweb/common/util/compress/TarDecompressor.java
@@ -0,0 +1,95 @@
+/**
+ * Copyright (c) 2017, Stupid Bird and/or its affiliates. All rights reserved.
+ * STUPID BIRD PROPRIETARY/CONFIDENTIAL. Use is subject to license terms.
+ *
+ * @Project : common
+ * @Package : net.lizhaoweb.common.util.compress
+ * @author <a href="http://www.lizhaoweb.net">李召(John.Lee)</a>
+ * @EMAIL 404644381@qq.com
+ * @Time : 20:36
+ */
+package net.lizhaoweb.common.util.compress;
+
+import org.apache.commons.compress.archivers.tar.TarArchiveEntry;
+import org.apache.commons.compress.archivers.tar.TarArchiveInputStream;
+import org.apache.commons.io.IOUtils;
+
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
+
+/**
+ * <h1>解压缩工具 - Tar</h1>
+ *
+ * @author <a href="http://www.lizhaoweb.cn">李召(John.Lee)</a>
+ * @version 1.0.0.0.1
+ * @EMAIL 404644381@qq.com
+ * @notes Created on 2017年08月10日<br>
+ * Revision of last commit:$Revision$<br>
+ * Author of last commit:$Author$<br>
+ * Date of last commit:$Date$<br>
+ */
+public class TarDecompressor extends AbstractCompressOrDecompress {
+
+    public TarDecompressor(boolean verbose) {
+        super(verbose);
+    }
+
+    /**
+     * 解压
+     *
+     * @param tarFile 压缩文件
+     * @param tarDir  目标目录
+     * @throws Exception 异常
+     */
+    public void decompressor(String tarFile, String tarDir) throws Exception {
+        this.decompressor(new File(tarFile), new File(tarDir));
+    }
+
+    /**
+     * 解压
+     *
+     * @param tarFile 压缩文件
+     * @param tarDir  目标目录
+     * @throws Exception 异常
+     */
+    public void decompressor(File tarFile, File tarDir) throws Exception {
+        FileInputStream fileInputStream = null;
+        TarArchiveInputStream tarArchiveInputStream = null;
+        try {
+            this.checkAndMakeDirectory(tarDir);
+            fileInputStream = new FileInputStream(tarFile);
+            tarArchiveInputStream = new TarArchiveInputStream(fileInputStream);
+            TarArchiveEntry tarArchiveEntry = null;
+
+            while ((tarArchiveEntry = tarArchiveInputStream.getNextTarEntry()) != null) {
+                File zipFileOrDir = new File(tarDir, tarArchiveEntry.getName());
+                if (tarArchiveEntry.isDirectory()) {
+                    this.checkAndMakeDirectory(zipFileOrDir);
+                    continue;
+                }
+                this.checkAndMakeDirectory(zipFileOrDir.getParentFile());
+                FileOutputStream fileOutputStream = null;
+                try {
+                    fileOutputStream = new FileOutputStream(zipFileOrDir);
+                    IOUtils.copy(tarArchiveInputStream, fileOutputStream);
+                    fileOutputStream.flush();
+                } finally {
+                    IOUtils.closeQuietly(fileOutputStream);
+                }
+                this.printInformation(String.format("The file[%s] decompression successful", zipFileOrDir));
+            }
+        } finally {
+            IOUtils.closeQuietly(tarArchiveInputStream);
+            IOUtils.closeQuietly(fileInputStream);
+        }
+        this.printInformation(String.format("The file[%s] has been unpacked to the directory[%s]", tarFile, tarDir));
+    }
+
+    private void checkAndMakeDirectory(File zipFileOrDir) {
+        if (!zipFileOrDir.exists()) {
+            boolean mkdir = zipFileOrDir.mkdirs();
+            this.printInformation(String.format("The directory[%s] is created : %s", zipFileOrDir, mkdir));
+        }
+    }
+}
