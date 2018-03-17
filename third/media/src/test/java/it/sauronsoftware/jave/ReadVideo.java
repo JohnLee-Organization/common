f8114682def8d33276918df22dcf6ef0196f5b5a
lz958942 <404644381@qq.com>
2018-03-17 12:17:02 +0800

    增加媒体转换插件

---
+++ third/media/src/test/java/it/sauronsoftware/jave/ReadVideo.java
@@ -0,0 +1,24 @@
+package it.sauronsoftware.jave;
+
+import java.io.File;
+
+/**
+ * Created by Administrator on 2018/3/17 0017.
+ */
+public class ReadVideo {
+    public static void main(String[] args) {
+        File source = new File("D:\\AV\\HardX - Eager To Please - Kylie Page.mp4");
+        Encoder encoder = new Encoder();
+        try {
+            MultimediaInfo m = encoder.getInfo(source);
+            long ls = m.getDuration();
+            int millisecond = (int) (ls % 1000);
+            int second = (int) (ls / 1000 % 60);
+            int minute = (int) (ls / 60000 % 60);
+            int hour = (int) (ls / 3600000);
+            System.out.println(String.format("此视频时长为:%d小时%d分%d秒%d毫秒！", hour, minute, second, millisecond));
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+}
