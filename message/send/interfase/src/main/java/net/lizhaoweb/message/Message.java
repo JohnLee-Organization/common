22a257425814f8ccac3d899c53ae03274198741e
lz958942 <404644381@qq.com>
2020-01-18 16:18:19 +0800

    增加消息发送模块

---
+++ message/send/interfase/src/main/java/net/lizhaoweb/message/Message.java
@@ -0,0 +1,33 @@
+/**
+ * Copyright (c) 2020, Stupid Bird and/or its affiliates. All rights reserved.
+ * STUPID BIRD PROPRIETARY/CONFIDENTIAL. Use is subject to license terms.
+ *
+ * @Project : common
+ * @Package : net.lizhaoweb.message
+ * @author <a href="http://www.lizhaoweb.net">李召(John.Lee)</a>
+ * @EMAIL 404644381@qq.com
+ * @Time : 11:08
+ */
+package net.lizhaoweb.message;
+
+import lombok.Data;
+
+/**
+ * <H1>模型 - 消息</H1>
+ *
+ * @author <a href="http://www.lizhaoweb.cn">李召(John.Lee)</a>
+ * @version 1.0.0.0.1
+ * @EMAIL 404644381@qq.com
+ * @notes Created on 2020年01月18日<br>
+ * Revision of last commit:$Revision$<br>
+ * Author of last commit:$Author$<br>
+ * Date of last commit:$Date$<br>
+ */
+@Data
+public class Message {
+    private String from;// 来源
+    private String to;// 目的地
+    private String title;// 标题
+    private String content;// 内容
+    private String enclosure;// 附件
+}
